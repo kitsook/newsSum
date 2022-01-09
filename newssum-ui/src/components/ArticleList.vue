81cee16af814b5dab6a50731a385d7677e6d8421
Clarence Ho <clarenceho@gmail.com>
2022-01-09 00:12:57 -0800

    cosmetic changes

--- newssum-ui/src/components/ArticleList.vue
+++ newssum-ui/src/components/ArticleList.vue
@@ -3,7 +3,7 @@
     <a v-for="(article, index) in articles" :key="index" :href="article.url"
         :class="article.url? (index%2 == 0? 'list-group-item-secondary' : 'list-group-item-light') : 'active'"
         class="list-group-item list-group-item-action" target="_blank">
-      <div :class="article.url? 'text-primary' : ''">{{ article.title }}</div>
+      <div :class="article.url? 'text-primary' : ''">{{ article.title? article.title.replace(re, '') : '' }}</div>
       <div>{{ article.abstract? article.abstract.replace(re, '') : '' }}</div>
     </a>
   </div>
@@ -12,13 +12,8 @@
 <script lang="ts">
 import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import NewsArticle from "../models/NewsArticle";
-import Loading from "../components/Loading.vue";
 
-@Component({
-  components: {
-    Loading,
-  },
-})
+@Component
 export default class ArticleList extends Vue {
   @Prop({ default: [] as NewsArticle[] }) articles!: NewsArticle[];
 
