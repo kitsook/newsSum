588039cca5222afc1bc31ad658770f0a766ab34f
Clarence Ho <clarenceho@gmail.com>
2024-12-20 18:08:17 -0800

    add suggestions

--- newssum-ui/src/models/NewsArticle.ts
+++ newssum-ui/src/models/NewsArticle.ts
@@ -1,5 +1,8 @@
+import Suggestion from "../models/Suggestion";
+
 export default interface NewsArticle {
   title: string;
   url: string;
   abstract: string;
+  suggestions: Suggestion[];
 }
--- setenv.sh
+++ setenv.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 export PATH=$PATH:/home/cho/apps/google-cloud-sdk/bin
+export CLOUDSDK_CORE_PROJECT="news-sum"
