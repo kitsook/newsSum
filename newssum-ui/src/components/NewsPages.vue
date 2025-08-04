c7396bbf3a0350b1b42afb575e06affc5353084c
Clarence Ho <clarenceho@gmail.com>
2025-08-03 22:04:24 -0700

    fix scrolling tab when first loaded

--- newssum-ui/src/components/NewsPages.vue
+++ newssum-ui/src/components/NewsPages.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="tabbable" ref="tabheaders">
-    <b-tabs content-class="" id="news-tabs" v-model="showTabIndex" @activate-tab="tabChanged" lazy>
+  <div class="tabbable">
+    <b-tabs content-class="" id="news-tabs" v-model="showTabIndex" @activate-tab="tabChanged" ref="newsTabs" lazy>
       <IndexTab :subscriptions="subscriptions"
           :sources="sources"
           @subscriptionChanged="subscriptionChanged"/>
@@ -48,10 +48,6 @@ export default class NewsPages extends Vue {
   showTabIndex = 0;
   firstTabChange = true;
 
-  $refs!: {
-    tabheaders: HTMLElement
-  }
-
   @Watch('subscriptions')
   onSubScriptionsChanged(newSubscriptions: Set<string>, oldSubscriptions: Set<string>) {
     this.refreshShowingSources(newSubscriptions, this.sources);
@@ -65,47 +61,14 @@ export default class NewsPages extends Vue {
   private tabChanged(newTabIndex: number, prevTabIndex: number, bvEvent: BvEvent) {
     if (this.firstTabChange) {
       this.firstTabChange = false;
-      if (newTabIndex > 0 && this.showingSources.length > newTabIndex) {
-        this.scrollToShowActiveTab(this.showingSources[newTabIndex].desc);
+      const theTabBar = this.$refs.newsTabs as Vue;
+      const theActiveTab = theTabBar.$el.querySelector(`a[aria-posinset="${newTabIndex+1}"]`) as Element;
+      if (theActiveTab) {
+        theActiveTab.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
       }
-    } else {
-      if (newTabIndex > 0 && this.showingSources.length > newTabIndex-1) {
+    } else if (newTabIndex > 0 && this.showingSources.length > newTabIndex-1) {
         Subscriptions.setLastRead(this.showingSources[newTabIndex-1].path);
-      }
     }
-  }
-
-  private scrollToShowActiveTab(tabText: string) {
-    const headers = this.$refs.tabheaders.querySelector(".nav-tabs");
-    let makeVisibleTab: HTMLElement | null = null;
-
-    const links = this.$refs.tabheaders.getElementsByClassName('nav-link');
-    for (let i = 0; i < links.length; i++) {
-      if (links[i].innerHTML === tabText) {
-        makeVisibleTab = links[i==links.length-1? i : i+1].parentElement; // get next tab
-        break;
-      }
-    }
-
-    // TODO find a more efficient and accurate way to scroll to the active tab
-    if (headers && makeVisibleTab) {
-      let maxScrollLeft = headers.scrollWidth - headers.clientWidth;
-      let scrollStep = maxScrollLeft / 5;
-      headers.scrollLeft = 0;
-      while(!this.isInViewport(makeVisibleTab) && headers.scrollLeft < maxScrollLeft) {
-        headers.scrollLeft += scrollStep;
-      }
-    }
-  }
-
-  private isInViewport(element: Element) {
-    const rect = element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
   }
 
   private subscriptionChanged() {
